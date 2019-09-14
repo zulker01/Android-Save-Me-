f25864aa7c6ab44d5c407b7ee3276af4e02c0735
zulker01 <zulker02@gmail.com>
2019-09-14 23:13:57 +0600

    front Page edited, added menu page at 11:13pm 14/09/19

--- app/src/main/java/com/example/saveme/MainActivity.java
+++ app/src/main/java/com/example/saveme/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.saveme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static java.lang.System.exit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button menuButton = (Button) findViewById(R.id.homeMenuButton);
+        Button quitButton = (Button) findViewById(R.id.homeQuitButton);
+
+
+        menuButton.setOnClickListener(new View.OnClickListener()
+                                      {
+                                          @Override
+                                          public void onClick(View view)
+                                          {
+                                              Toast menuToast = Toast.makeText(MainActivity.this,"pak pak", Toast.LENGTH_LONG);
+                                              menuToast.show();
+
+                                              Intent intent = new Intent(MainActivity.this, menuPage.class);
+                                              //intent.putExtra("pak",100);
+
+                                              startActivity(intent);
+                                          }
+                                      }
+        );
+        quitButton.setOnClickListener(new View.OnClickListener()
+                                      {
+                                          @Override
+                                          public void onClick(View view)
+                                          {
+                                              Toast quitToast = Toast.makeText(MainActivity.this,"mara kha", Toast.LENGTH_LONG);
+                                              quitToast.show();
+                                              exit(0);
+                                          }
+                                      }
+        );
     }
 }
