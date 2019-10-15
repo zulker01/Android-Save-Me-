fa4428ad5bc8d785dfc50b83c25838be3412aabb
Borhan20 <borhanuddin.24csedu.020@gmail.com>
2019-10-15 11:25:54 +0600

    database helper class created

---
+++ app/src/main/java/com/example/saveme/ShowContactsActivity.java
@@ -0,0 +1,15 @@
+package com.example.saveme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_contacts);
+
+    }
+}
