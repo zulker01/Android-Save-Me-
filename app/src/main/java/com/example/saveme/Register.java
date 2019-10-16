236db4d8182e4603c993b9f1267ffe8956d97a8c
mahodiatik <mahodiatikshuvo@gmail.com>
2019-10-16 10:30:03 +0600

    Registration finalization for all numbers

--- app/src/main/java/com/example/saveme/Register.java
+++ app/src/main/java/com/example/saveme/Register.java
@@ -18,16 +18,41 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Button submit1=findViewById(R.id.submit1)    ;
+        Button submit1=findViewById(R.id.submit1);
+        Button submit2=findViewById(R.id.submit2) ;
+        Button submit3=findViewById(R.id.submit3);
 
 
         submit1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final EditText edt = (EditText)findViewById(R.id.editumber1);
-                final String number1 = edt.getText().toString();
+                final EditText edt1 = (EditText)findViewById(R.id.editnumber1);
+                final String number1 = edt1.getText().toString();
                 Toast.makeText(Register.this, "Added Number: "+number1, Toast.LENGTH_LONG).show();
                 saveFile("number1.txt",number1);
+
+            }
+        });
+
+        submit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText edt2 = (EditText)findViewById(R.id.editnumber2);
+                final String number2 = edt2.getText().toString();
+                Toast.makeText(Register.this, "Added Number: "+number2, Toast.LENGTH_LONG).show();
+                saveFile("number2.txt",number2);
+
+            }
+        });
+
+        submit3
+                .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText edt3 = (EditText)findViewById(R.id.editnumber3);
+                final String number3 = edt3.getText().toString();
+                Toast.makeText(Register.this, "Added Number: "+number3, Toast.LENGTH_LONG).show();
+                saveFile("number2.txt",number3);
 
             }
         });
