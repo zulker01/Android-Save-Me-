934b95eebc0b7999d3827a888dd13785fa139511
mahodiatik <mahodiatikshuvo@gmail.com>
2019-11-13 14:29:43 +0600

    Added some notification feature

--- app/src/main/java/com/example/saveme/NotificationReceiver.java
+++ app/src/main/java/com/example/saveme/NotificationReceiver.java
@@ -12,8 +12,13 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -26,6 +31,14 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static com.example.saveme.MainActivity.latitude;
+import static com.example.saveme.MainActivity.longitude;
+
 
 public class NotificationReceiver extends AppCompatActivity {
 
@@ -37,12 +50,34 @@ public class NotificationReceiver extends AppCompatActivity {
         callNow();
         sendLoction();
         MainActivity.makezero();
+        sendNotification();
+
+      /* try {
+           start();
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+
+
+        Button stop = (Button) findViewById(R.id.btnstop);
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                }
+            }
+        });
+
+       */
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_receiver);
 
-        mulai = (Button) findViewById(R.id.btnmenu);
 
-        mulai.setOnClickListener(new View.OnClickListener() {
+        Button menu = (Button) findViewById(R.id.btnmenu);
+
+        menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(NotificationReceiver.this, MainActivity.class);
@@ -52,42 +87,141 @@ public class NotificationReceiver extends AppCompatActivity {
     }
 
 
-
-
-    public void callNow()
-    {
-        String number="01785373724";
+    public void callNow() {
+        String number = "01785373724";
         Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:"+number));
+        callIntent.setData(Uri.parse("tel:" + number));
 
-        if( ActivityCompat.checkSelfPermission(NotificationReceiver.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED)
-        {
+        if (ActivityCompat.checkSelfPermission(NotificationReceiver.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             Toast menuToast = Toast.makeText(NotificationReceiver.this, R.string.phonePermission, Toast.LENGTH_LONG);
             menuToast.show();
-            return ;
+            return;
         }
         startActivity(callIntent);
     }
 
-    public void sendLoction()
-    {
-        String phoneNumber="785373724";
-        String smsMessage = "I am in danger, HELP ! \n\n I am at " +"\n Latitude : "+MainActivity.latitude+"\nLongitude :  "+MainActivity.longitude+" ";
+    public void sendLoction() {
+        String phoneNumber = "785373724";
+        String smsMessage = "I am in danger, HELP ! \n\n I am at " + "\n Latitude : " + latitude + "\nLongitude :  " + longitude + getCompleteAddressString(latitude,longitude);
 
-        if(checkPermission(Manifest.permission.SEND_SMS)){
+        if (checkPermission(Manifest.permission.SEND_SMS)) {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNumber, null, smsMessage, null, null);
             Toast.makeText(NotificationReceiver.this, "Message Sent!", Toast.LENGTH_SHORT).show();
-        }else {
+        } else {
             Toast.makeText(NotificationReceiver.this, "Permission Denied", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public boolean checkPermission(String permission){
+    public boolean checkPermission(String permission) {
         int check = ContextCompat.checkSelfPermission(NotificationReceiver.this, permission);
         return (check == PackageManager.PERMISSION_GRANTED);
     }
+
+    public void sendNotification()
+    {
+        Intent intent1 = new Intent(getApplicationContext(),NotificationReceiver.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder b = new NotificationCompat.Builder(getApplicationContext());
+
+        b.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_notification)
+                .setTicker("Hearty365")
+                .setContentTitle("Are you safe?")
+                .setContentText("If not Tap Here")
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                .setContentIntent(contentIntent)
+                .setContentInfo("Info");
+
+
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, b.build());
+    }
+
+
+    public String getCompleteAddressString(double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+                //Log.w("My Current loction address", strReturnedAddress.toString());
+            } else {
+                //Log.w("My Current loction address", "No Address returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Log.w("My Current loction address", "Canont get Address!");
+        }
+        return strAdd;
+    }
 }
+
+
+    /*final static MediaRecorder recorder = new MediaRecorder();
+    public static  String path;
+
+    public NotificationReceiver(String path) {
+        this.path = sanitizePath(path);
+    }
+
+    public static String sanitizePath(String path) {
+        if (!path.startsWith("/")) {
+            path = "/" + path;
+        }
+        if (!path.contains(".")) {
+            path += ".3gp";
+        }
+        return Environment.getExternalStorageDirectory().getAbsolutePath()
+                + path;
+    }
+
+    public static void start() throws IOException {
+        String state = android.os.Environment.getExternalStorageState();
+        if (!state.equals(android.os.Environment.MEDIA_MOUNTED)) {
+            throw new IOException("SD Card is not mounted.  It is " + state
+                    + ".");
+        }
+
+        // make sure the directory we plan to store the recording in exists
+        File directory = new File(path).getParentFile();
+        if (!directory.exists() && !directory.mkdirs()) {
+            throw new IOException("Path to file could not be created.");
+        }
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(path);
+        recorder.prepare();
+        recorder.start();
+    }
+
+    public static void stop() throws IOException {
+        recorder.stop();
+        recorder.release();
+    }
+
+    public static void playarcoding(String path) throws IOException {
+        MediaPlayer mp = new MediaPlayer();
+        mp.setDataSource(path);
+        mp.prepare();
+        mp.start();
+        mp.setVolume(10, 10);
+    }
+}
+
+     */
 
 
 
